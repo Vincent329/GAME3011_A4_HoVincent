6b1ff6298be0cdd386246056f5b0d7089a3f177b
Vincent329 <vincee.ho@gmail.com>
2022-04-04 12:48:17 -0400

    [ADD] - Movement Input for player

--- GAME3011_A4/Assets/_Scripts/Managers/InputManager.cs
+++ GAME3011_A4/Assets/_Scripts/Managers/InputManager.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    public static PlayerInputData playerInputActions;
+    public static event Action<InputActionMap> actionMapChange;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputData();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ToggleActionMap(playerInputActions.Player);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void ToggleActionMap(InputActionMap inputMap)
     {
-        
+        Debug.Log("SwitchTo... " + inputMap);
+        if (inputMap.enabled)
+            return;
+
+        playerInputActions.Disable();
+        actionMapChange?.Invoke(inputMap);
+        inputMap.Enable();
     }
 }
--- GAME3011_A4/Assets/_Scripts/PlayerMovement.cs
+++ GAME3011_A4/Assets/_Scripts/PlayerMovement.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+    private PlayerInputData inputDataRef;
+
+    bool isActive = false;
+
+    // Movement Variables
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private Vector2 moveVector;
+    [SerializeField] private Vector3 playerVelocity;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
-        
+        inputDataRef = InputManager.playerInputActions;
+
+        isActive = true;
+        inputDataRef.Player.Move.performed += OnMove;
+        inputDataRef.Player.Move.canceled += OnMove;
+    }
+
+    private void OnEnable()
+    {
+        if (isActive)
+        {
+            inputDataRef.Player.Move.performed += OnMove;
+            inputDataRef.Player.Move.canceled += OnMove;
+        }
+    }
+
+    private void OnDisable()
+    {
+        inputDataRef.Player.Move.performed -= OnMove;
+        inputDataRef.Player.Move.canceled -= OnMove;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.MovePosition(rb.position + playerVelocity * playerSpeed * Time.deltaTime);
+    }
+
+    private void OnMove(InputAction.CallbackContext obj)
+    {
+        moveVector = obj.ReadValue<Vector2>();
+        Debug.Log(moveVector);
+        playerVelocity = new Vector3(moveVector.x, 0, moveVector.y);
+    }
+
+    private void StartMinigame()
+    {
+        InputManager.ToggleActionMap(inputDataRef.Minigame);
     }
 }
